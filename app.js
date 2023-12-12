50c9be445041d9e25a5d31e0cd92644abeec6eee
Rakesh Reddy Kovvuri <125377731+RakeshReddy8@users.noreply.github.com>
2023-12-12 19:57:54 +0530

    we commitsaa

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-app.get('/', (req, res) => res.send('Hello, Jenkins Pipeline! ESNE Project with Jenkins WebHook and Rolling Update change'));
+app.get('/', (req, res) => res.send('Hello, Jenkins Pipeline! ESNE Project with Jenkins WebHook and Rolling Update 50 and 100'));
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
